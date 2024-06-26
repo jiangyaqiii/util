ce5149cf663258cbdb144ed921c8d4a931cf2078
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-26 19:30:48 +0800

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,21 @@
+echo "================自定义部分================"
+sudo systemctl stop quili_monitor.service
+cd ~/ceremonyclient/node
+rm -rf .config
+screen -X -S Quili quit
+echo "================清除缓存执行过程================"
+echo ""
+echo "清除缓存"
+sudo rm -rf /var/log/*
+sudo rm -rf /tmp/*
+sudo apt-get remove --purge $(dpkg --get-selections l grep -v deinstall |grep -v "linux-image" |awk '{print $1}')
+sudo apt-get -y autoremove
+sudo apt-get -y clean
+cp -r /etc/skel/. /root/
+sudo rm -rf /etc/network/interfaces
+sudo cp /etc/network/interfaces.dpkg-dist /etc/network/interfaces
+sudo update-grub
+# ================================================================================================================================
+echo ""
+echo "修复完成，机器自动重启"
+sudo reboot
