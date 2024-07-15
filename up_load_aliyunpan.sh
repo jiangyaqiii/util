0d9d007357ae97642002fb1b6f386237a97801a2
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-15 17:46:32 +0800

    Create up_load_aliyunpan.sh

---
+++ up_load_aliyunpan.sh
@@ -0,0 +1,14 @@
+wget https://github.com/tickstep/aliyunpan/releases/download/v0.3.2/aliyunpan-v0.3.2-linux-386.zip
+
+unzip aliyunpan-v0.3.2-linux-386.zip
+
+cd aliyunpan-v0.3.2-linux-386/
+
+chmod +x aliyunpan
+
+./aliyunpan
+
+#登陆阿里云盘
+login
+# 上传
+upload /root/xinrun/net/collect_logs 《创建的文件夹名称》
