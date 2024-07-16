3c768ee44b8eaa72d066d850977944551bdc0786
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-17 02:03:35 +0800

    Update up_oss.sh

--- up_oss.sh
+++ up_oss.sh
@@ -1,14 +1,14 @@
 cd ~
 echo '[Credentials]
-language = CH' > config
+language = CH' > .ossutilconfig
 
-echo endpoint=$endpoint>>config
-echo accessKeyID=$accessKeyID>>config
-echo accessKeySecret=$accessKeySecret>>config
+echo endpoint=$endpoint>>.ossutilconfig
+echo accessKeyID=$accessKeyID>>.ossutilconfig
+echo accessKeySecret=$accessKeySecret>>.ossutilconfig
 
 wget http://gosspublic.alicdn.com/ossutil/1.7.1/ossutil64 -O ossutil
 chmod +x ossutil
 mv ossutil /usr/bin/ossutil
-ossutil config --config-file=/root/config.conf
+# ossutil config --config-file=/root/config
 
 ossutil cp -r /root/ceremonyclient/node/.config oss://xin-run/$remote_addr
